3c506332f068a7f19717cc5cd4dc1ac73cc77290
Sam Bauch <sbauch@gmail.com>
2017-10-23 10:26:51 -0400

    Adds Client.authenticate and Client::build, a convenience initializer

--- lib/todoable/client.rb
+++ lib/todoable/client.rb
@@ -1,10 +1,23 @@
 module Todoable
   class Client
 
+    attr_accessor :token
     attr_writer :username, :password
 
+    # TODO: document this
     def initialize(username: , password: )
       @username, @password = username, password
+    end
+
+    # TODO: document this
+    def authenticate
+      # TODO: implement calling the API
+      @token = 'this-should-be-a-real-token'
+    end
+
+    # TODO: document this
+    def self.build(username: , password: )
+      new(username: username, password: password).authenticate
     end
 
   end
