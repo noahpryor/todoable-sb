337e916a4bee1a8f151c27439a55f174244c433d
Sam Bauch <sbauch@gmail.com>
2017-10-23 10:26:51 -0400

    Adds Client.authenticate and Client::build, a convenience initializer

--- lib/todoable/sb.rb
+++
@@ -1,5 +0,0 @@
-require "todoable/version"
-
-module Todoable
-  
-end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -4,7 +4,7 @@ describe Todoable::Client do
   let (:username) { "username" }
   let (:password) { "password" }
 
-  describe "#new" do
+  describe "::new" do
 
     it "raises an error when initiliazed without a username" do
       expect{ Todoable::Client.new(password: password) }.to raise_error(ArgumentError)
@@ -18,6 +18,56 @@ describe Todoable::Client do
       client = Todoable::Client.new(username: username, password: password)
       expect(client).to be_a Todoable::Client
     end
+
+    context "with an initialized client" do
+
+      let (:client) { Todoable::Client.new(username: username, password: password)}
+
+      describe "#authenticate" do
+
+        xit "calls the authenticate endpoint" do
+
+        end
+
+        context "receives a successful response" do
+
+          it "sets the authentication token" do
+              client.authenticate
+              expect(client.token).to be_a(String)
+          end
+        end
+
+        context "receives an error" do
+
+        end
+      end
+
+
+
+    end
+
+    describe "::build" do
+
+      let (:client){ Todoable::Client.new(username: username, password: password) }
+
+      it "raises an error when called without a username" do
+        expect{ Todoable::Client.build(password: password) }.to raise_error(ArgumentError)
+      end
+
+      it "raises an error when called without a password" do
+        expect{ Todoable::Client.build(username: username) }.to raise_error(ArgumentError)
+      end
+
+      it "initializes and authenticates a client with username and password" do
+        expect(Todoable::Client).to receive(:new).with({username: username, password: password})
+                                                 .and_return(client)
+        expect(client).to receive(:authenticate).once
+        Todoable::Client.build(username: username, password: password)
+      end
+
+
+    end
+
   end
 
 end
